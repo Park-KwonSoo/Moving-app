270b357d1a7ef64401df7a224ba971039cb6dec2
Kyubum Jang <jkb2221@gmail.com>
2021-12-19 09:38:25 +0900

    CLN: 차트프레젠터 뷰 구성

--- src/components/musicChart/MusicChartPresenter.tsx
+++ src/components/musicChart/MusicChartPresenter.tsx
@@ -1,33 +1,135 @@
 import React from 'react';
-import { NavigationProp, RouteProp } from '@react-navigation/native';
+import {NavigationProp, RouteProp} from '@react-navigation/native';
+
+// import { Button } from 'react-native-elements';
 
 import {
-    SafeAreaView,
-    ScrollView,
-    View,
-    TouchableOpacity,
-    Text,
+  SafeAreaView,
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Image,
+  Button,
+  Text,
 } from 'react-native';
 
 import styles from './MusicChartStyled';
 
-
-
 interface MusicChartProps {
-    navigation : NavigationProp<{}>;
-    route : RouteProp<{}>;
+  navigation: NavigationProp<{}>;
+  route: RouteProp<{}>;
 }
 
-const MusicChartPresenter = ({ navigation, route, ...props } : MusicChartProps) => {
-    return (
-        <SafeAreaView style = {styles.container}>
-        <View style = {styles.wrapper}>
-            <ScrollView>
-                <Text>차트 컨테이너</Text>
-            </ScrollView>
+const MusicChartPresenter = ({
+  navigation,
+  route,
+  ...props
+}: MusicChartProps) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <View style={styles.headerContainer}>
+          <Button
+            // onPress={() => alert('차트 보기')}
+            title="차트"
+            color="#574B90"
+          />
+          <Button
+            // onPress={() => alert('실시간 급상승')}
+            title="실시간 급상승"
+            color="#574B90"
+          />
+          <Button
+            // onPress={() => alert('장르 테마')}
+            title="장르 테마"
+            color="#574B90"
+          />
+          <Button
+            // onPress={() => alert('상황')}
+            title="상황"
+            color="#574B90"
+          />
         </View>
-        </SafeAreaView>
-    );
+        <ScrollView>
+          <View style={styles.chartContainer}>
+            <Text style={styles.chartContainerTitle}>찾아 보기 &gt;</Text>
+            <Text style={styles.chartContainerTitle}>Moving 차트</Text>
+            <Text style={styles.chartContainerSubTitle}>
+              Moving 최고 인기곡 차트
+            </Text>
+            <View style={styles.chartRankingContainer}>
+              <Image
+                style={styles.albumImg}
+                source={require('../../../assets/images/album_6.jpg')}
+              />
+              <Text style={styles.rankingTitle}> 1 </Text>
+              <View style={styles.rankingTitleContainer}>
+                <Text style={styles.rankingTitle}> Counting Stars </Text>
+                <Text style={styles.rankingSubTitle}> BE'O </Text>
+              </View>
+            </View>
+            <View style={styles.chartRankingContainer}>
+              <Image
+                style={styles.albumImg}
+                source={require('../../../assets/images/album_horse.jpg')}
+              />
+              <Text style={styles.rankingTitle}> 2 </Text>
+              <View style={styles.rankingTitleContainer}>
+                <Text style={styles.rankingTitle}> 회전목마 </Text>
+                <Text style={styles.rankingSubTitle}> sokodomo </Text>
+              </View>
+            </View>
+            <View style={styles.chartRankingContainer}>
+              <Image
+                style={styles.albumImg}
+                source={require('../../../assets/images/album-limousine.jpg')}
+              />
+              <Text style={styles.rankingTitle}> 3 </Text>
+              <View style={styles.rankingTitleContainer}>
+                <Text style={styles.rankingTitle}> 리무진 </Text>
+                <Text style={styles.rankingSubTitle}> BE'O </Text>
+              </View>
+            </View>
+            <View style={styles.chartRankingContainer}>
+              <Image
+                style={styles.albumImg}
+                source={require('../../../assets/images/album-eleven.jpg')}
+              />
+              <Text style={styles.rankingTitle}> 4 </Text>
+              <View style={styles.rankingTitleContainer}>
+                <Text style={styles.rankingTitle}> Eleven </Text>
+                <Text style={styles.rankingSubTitle}> IVE(아이브) </Text>
+              </View>
+            </View>
+            <View style={styles.chartRankingContainer}>
+              <Image
+                style={styles.albumImg}
+                source={require('../../../assets/images/album_9.jpg')}
+              />
+              <Text style={styles.rankingTitle}> 5 </Text>
+              <View style={styles.rankingTitleContainer}>
+                <Text style={styles.rankingTitle}>
+                  만남은 쉽고 이별은 어려워
+                </Text>
+                <Text style={styles.rankingSubTitle}> 베이식 </Text>
+              </View>
+            </View>
+            <View style={styles.chartRankingContainer}>
+              <Image
+                style={styles.albumImg}
+                source={require('../../../assets/images/album_1.jpg')}
+              />
+              <Text style={styles.rankingTitle}> 6 </Text>
+              <View style={styles.rankingTitleContainer}>
+                <Text style={styles.rankingTitle}>매일 크리스마스</Text>
+                <Text style={styles.rankingSubTitle}> 다비치 </Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default MusicChartPresenter;
--- src/components/musicChart/MusicChartStyled.tsx
+++ src/components/musicChart/MusicChartStyled.tsx
@@ -1,22 +1,99 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container : {
-        backgroundColor : '#fff',
-        display : 'flex',
-        flexDirection : 'column',
-        alignItems : 'center',
-        justifyContent : 'center',
-        width : '100%',
-        height : '85%',
+  container: {
+    backgroundColor: '#fff',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '85%',
+  },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginBottom: '2%',
+  },
+  headerButton: {
+    backgroundColor: '#574B90',
+    alignItems: 'center',
+    fontColor: '#fff',
+    justifyContent: 'center',
+    width: 70,
+    height: 70,
+    marginBottom: 30,
+    borderRadius: 35,
+  },
+  chartContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    margin: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: '#ffffff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-
-    wrapper : {
-        width : '100%',
-        height : '85%',
-        marginBottom : '15%',
-    },
-
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  chartContainerTitle: {
+    marginTop: '4%',
+    marginLeft: '2%',
+    marginBottom: '2%',
+    fontFamily: 'Jua-Regular',
+    fontSize: 18,
+    color: '#000',
+    fontWeight: '300',
+  },
+  chartContainerSubTitle: {
+    marginLeft: '2%',
+    marginBottom: '2%',
+    fontFamily: 'Jua-Regular',
+    fontSize: 14,
+    color: '#C4C4C4',
+    fontWeight: '300',
+  },
+  chartRankingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '2%',
+  },
+  albumImg: {
+    marginLeft: '2%',
+    width: 50,
+    height: 50,
+  },
+  rankingTitle: {
+    marginLeft: '4%',
+    marginRight: '4%',
+    fontFamily: 'Cafe24SsurroundAir',
+    fontSize: 18,
+    fontWeight: '300',
+  },
+  rankingSubTitle: {
+    marginLeft: '5%',
+    marginRight: '4%',
+    fontFamily: 'Jua-Regular',
+    fontSize: 14,
+    fontWeight: '300',
+    color: '#C4C4C4',
+  },
+  rankingTitleContainer: {
+    flexDirection: 'column',
+  },
+  wrapper: {
+    width: '100%',
+    height: '85%',
+    marginBottom: '15%',
+  },
 });
 
 export default styles;
